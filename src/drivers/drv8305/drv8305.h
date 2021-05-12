8cbe95e6a55d3167e0a5fc7f7dd1b7abee727bec
maxime <mlemonnier@neuralium.com>
2021-05-12 12:03:06 -0600

    move to .cpp

--- src/drivers/drv8305/drv8305.h
+++ src/drivers/drv8305/drv8305.h
@@ -15,34 +15,8 @@ public:
 
     void init(SPIClass* spi = &SPI);
 
-    void refresh()
-    {
-        warningAndWatchdog.poll();
-        oVandVDSFaults.poll();
-        icFaults.poll();
-        gateDriverVGSFaults.poll();
-        hsGateControl.poll();
-        lsGateControl.poll();
-        gateDriveControl.poll();
-        icOperation.poll();
-        shuntAmplifierControl.poll();
-        voltageRegulatorControl.poll();
-        vdsSenseControl.poll();
-    }
-    void report(Stream & stream, bool refresh = true)
-    {
-        warningAndWatchdog.report(stream, refresh);
-        oVandVDSFaults.report(stream, refresh);
-        icFaults.report(stream, refresh);
-        gateDriverVGSFaults.report(stream, refresh);
-        hsGateControl.report(stream, refresh);
-        lsGateControl.report(stream, refresh);
-        gateDriveControl.report(stream, refresh);
-        icOperation.report(stream, refresh);
-        shuntAmplifierControl.report(stream, refresh);
-        voltageRegulatorControl.report(stream, refresh);
-        vdsSenseControl.report(stream, refresh);
-    }
+    void refresh();
+    void report(Stream & stream, bool refresh = true);
 
     WarningAndWatchdog      warningAndWatchdog;
     OVandVDSFaults          oVandVDSFaults;
